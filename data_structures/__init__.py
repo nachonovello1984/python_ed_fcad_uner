ce581c71b5a3a2e1a6502e3d0ea0c2bec16c07aa
nacho <nacho.novello@gmail.com>
2022-10-16 08:45:29 -0300

    PriorityQueueBase "importable"

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -1,3 +1,3 @@
 from .linear import ArrayStack, LinkedStack, ArrayQueue, LinkedQueue, LinkedList, ListNode
 from .trees import BinaryTreeNode, LinkedBinaryTree
-from .priority_queues import ArrayHeap
+from .priority_queues import ArrayHeap, PriorityQueueBase
