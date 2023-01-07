d762338ab4200857c35431676629051cb06de1f6
nacho <nacho.novello@gmail.com>
2023-01-07 14:52:41 -0300

    Configuración de Coverage.py y de proyect root

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-ed-fcad-uner',
+    packages=find_packages(where='data_structures'),
+    package_dir={'': 'data_structures'}
+    description='',
+    version='1.0.0',
+    author='nacho <nacho.novello@gmail.com>'
+    install_requires=[],
+    extras_require={}
+)
